8fd4d7edf8b63217c037be3cd9fee6f288b362f4
zyx <1549583737@qq.com>
2019-04-28 00:49:09 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+    <div id="app">
+        <selectComponent :subject="subject" :selectContent="selectContent" :selectWidth="selectWidth"
+                         @changeSelect="cS"/>
+        <img src="./assets/logo.png">
+        <HelloWorld/>
+        <paginate :curr="1" :all="10"/>
+        <button @click="showToast()">点击显示Toast</button>
+        <toast :message='JSON.stringify("细思极恐")' :time="3000" ref="$Message"/>
+    </div>
+</template>
+
+<script>
+    import HelloWorld from './components/HelloWorld'
+    import selectComponent from './components/selectComponent'
+    import paginate from './components/paginate'
+    import toast from './components/toast'
+
+    export default {
+        name: 'App',
+        data() {
+            return {
+                subject: [{index: 0, text: "吃饭"}, {index: 1, text: "睡觉"}, {index: 2, text: "打豆豆"}],
+                selectContent: {
+                    value: 0,
+                    text: "点击选择"
+                },
+                selectWidth: 200
+            }
+        },
+        methods: {
+            cS(a, b) {
+                console.log(a);
+                console.log(b);
+            },
+            showToast() {
+                // this.$refs.toastUI.$emit('childMethod');
+                this.$refs.$Message.init()
+            }
+        },
+        components: {
+            HelloWorld,
+            selectComponent,
+            paginate,
+            toast
+        }
+    }
+</script>
+
+<style>
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
---
+++ src/components/toast.vue
@@ -0,0 +1,73 @@
+<template>
+    <div id="toastDiv">
+        <span class="message">{{message}}</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "toast",
+        components: {},
+        props: {
+            message: {
+                type: String,
+                default: ""
+            },
+            time: {
+                type: Number,
+                default: 3000
+            }
+        },
+        data() {
+            return {
+                odiv: "",
+                css: {
+                    // width: this.message.length <= 6 ? this.message.length * 2 + "px" : "auto",
+                    width: "auto",
+                    height: this.message.length <= 10 ? Math.ceil(this.message.length / 10) * 20 + "px" : 20 + "px",
+                    backgroundColor: "rgba(0, 0, 0, .8)",
+                    padding: "4px",
+                    margin: this.width ? "0 -" + this.width.split("px")[0] / 2 + "px" : "5px",
+                    position: "fixed",
+                    left: "50%",
+                    bottom: "50%",
+                    zIndex: "999",
+                    textAlign: "center",
+                    borderRadius: "5px",
+                    display: "block",
+                    verticalAlign: "middle"
+                }
+            }
+        },
+        mounted: function () {
+            // console.log(this.message.length);
+
+            // document.getElementsByTagName('body')[0].appendChild()
+            this.odiv = document.getElementById("toastDiv");
+            // this.$on('childMethod', function () {
+            //     alert("进来了");
+            // });
+            // this.init();
+        },
+        methods: {
+            init: function () {
+                for (var property in this.css) {
+                    this.odiv.style[property] = this.css[property];
+                }
+                this.stot();
+            },
+            stot: function () {
+                var that = this;
+                setTimeout(function () {
+                    that.odiv.style.display = "none";
+                }, that.time || 3000)
+            }
+        }
+    }
+</script>
+
+<style>
+    #toastDiv .message {
+        color: #fff
+    }
+</style> 
